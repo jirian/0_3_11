29725de01dee5c0a7b119aaed9717becc6873b0c
Garrett <garrett.kelly@patchcablemgr.com>
2020-07-03 23:34:28 +0000

    App.class: Build tree ports validate compatibility array is set

--- includes/App.class.php
+++ includes/App.class.php
@@ -1013,15 +1013,17 @@ var $qls;
 										$insertID = $insert['id'];
 										$insertName = $insert['name'];
 										$insertTemplateID = $insert['template_id'];
-										foreach($this->compatibilityArray[$insertTemplateID] as $insertFace => $insertFaceObj) {
-											foreach($insertFaceObj as $insertDepth => $insertPartition) {
-												
-												// Cannot be a trunked endpoint
-												if(!$this->peerArrayStandard[$insertID][0][$insertDepth]['selfEndpoint']) {
-													$separator = $insertPartition['partitionFunction'] == 'Endpoint' ? '' : '.';
-													$insertPartition['objectID'] = $insertID;
-													$insertPartition['portNamePrefix'] = $insertName == '' ? '' : $insertName.$separator;
-													array_push($elementArray, $insertPartition);
+										if(isset($this->compatibilityArray[$insertTemplateID])) {
+											foreach($this->compatibilityArray[$insertTemplateID] as $insertFace => $insertFaceObj) {
+												foreach($insertFaceObj as $insertDepth => $insertPartition) {
+													
+													// Cannot be a trunked endpoint
+													if(!$this->peerArrayStandard[$insertID][0][$insertDepth]['selfEndpoint']) {
+														$separator = $insertPartition['partitionFunction'] == 'Endpoint' ? '' : '.';
+														$insertPartition['objectID'] = $insertID;
+														$insertPartition['portNamePrefix'] = $insertName == '' ? '' : $insertName.$separator;
+														array_push($elementArray, $insertPartition);
+													}
 												}
 											}
 										}
